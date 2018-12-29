8a9fad81b5a7a4d952502da7bb6dbf607f3bf658
Brunno Vodola <bvodola@gmail.com>
2018-12-29 19:39:51 -0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import {Text} from 'react-native';
+export default () => <div><Text>App</Text></div>
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './public/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    path: path.resolve('build'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-env', '@babel/preset-react'] } }, exclude: /node_modules/ },
+      { test: /\.jsx$/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-env', '@babel/preset-react'] } }, exclude: /node_modules/ },
+    ]
+  },
+  devServer: {
+    port: 4000,
+    historyApiFallback: true,
+  },
+  plugins: [
+    HtmlWebpackPluginConfig,
+  ],
+  resolve: {
+    alias: {
+      'react-native$': 'react-native-web'
+    }
+  }
+};
